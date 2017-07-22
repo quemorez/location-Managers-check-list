85f9ef83e4602104e0979f049a8031fafe879600
quemorez <zacharyquemore@zacharys-macbook-pro.local>
2017-07-22 10:25:05 -0700

    Initial Commit

---
+++ LocationManagersCheckList no keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationManagersCheckList no keyboard
+//
+//  Created by Zachary Quemore on 7/22/17.
+//  Copyright © 2017 Zachary Quemore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
